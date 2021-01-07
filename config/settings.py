5b15e083e932cb332273f1ea165fc7ac961b5305
Felix Ha <felixha@protonmail.com>
2021-01-07 17:36:37 +0100

    reenable text compression

--- config/settings.py
+++ config/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'compression_middleware.middleware.CompressionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
