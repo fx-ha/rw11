3b3a682ba43676c971986c398b86a33d1f286fd3
Felix Ha <felixha@protonmail.com>
2021-11-28 18:02:45 +0000

    feat: :sparkles: change mail recipient for demo

--- apps/core/views.py
+++ apps/core/views.py
@@ -115,7 +115,7 @@ def contact(request):
                     body += ': '
                     body += message_note
 
-                recipients = ['rw11@uni-bayreuth.de']
+                recipients = ['felix@fxha.dev']
 
                 # wenn Email-Versand-Fehler
                 try:
