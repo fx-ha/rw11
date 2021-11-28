cf44bdf5c21044ed417238f9e2be76249761b99f
Felix Ha <felixha@protonmail.com>
2021-11-28 19:36:46 +0000

    add dj database url

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
